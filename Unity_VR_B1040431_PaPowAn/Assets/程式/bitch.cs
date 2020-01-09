dd2cf52fe50e0c2ec3e9b72b8e542feb52ddaebc
godanan91134 <55218484+godanan91134@users.noreply.github.com>
2020-01-09 14:13:28 +0800

    期末

---
+++ Unity_VR_B1040431_PaPowAn/Assets/程式/bitch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class bitch : MonoBehaviour
+{
+    public GameObject obj;
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.name == "Girl")
+        {
+            print("123");
+            obj.SetActive(true);
+        }
+    }
+}
---
+++ Unity_VR_B1040431_PaPowAn/Assets/程式/player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+
+    public int count;
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "武器")
+        {
+            count += 1;
+        }
+    }
+}
